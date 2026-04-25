836db216648853c07e630f20a94465b472a0b748
Befkadu <befkadu.beletee@gmail.com>
2026-04-25 21:13:20 +0300

    HOF

---
+++ HOF.js
@@ -0,0 +1,15 @@
+(function () {
+  const A = new Array(900000).fill("*");
+  console.log(A);
+  return A;
+})();
+
+// function F2() {
+//   console.log("Function 2");
+//   F1();
+// }
+// function F3() {
+//   console.log("Functiin 3");
+//   F2();
+// }
+// F3();
